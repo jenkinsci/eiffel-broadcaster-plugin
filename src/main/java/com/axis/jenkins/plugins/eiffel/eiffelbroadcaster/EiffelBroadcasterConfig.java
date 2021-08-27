jenkinsci/eiffel-broadcaster-plugin

commit e8171f625d302e19e3591f89aac3d4f41eb83b41

Validate outbound events
For general sanity but also to act as a safety mechanism for the future
pipeline step for publishing an event we validate all outbound events
against the available schemas. Events that fail the validation won't be
published (but their payload will be logged).

For now only the schemas bundled with the plugin are available but we
can easily extend this to allow plugins to contribute schema provider
that can be centrally configurable. That way sites can configure this
plugin to fetch schemas via e.g. HTTP. The primary reason of that would
be to avoid having to update the plugin just to get access to a new
event or event version. We currently bundle all events and versions
available in https://github.com/eiffel-community/eiffel at the
edition-paris tag.

The plan was to use com.github.java-json-tools:json-schema-validator
but it needs Guava 12 and Jenkins only includes Guava 11. To avoid
dependency hell we went with com.networknt:json-schema-validator instead.
It also uses Jackson so the API is nearly identical, its performance is
allegedly better but the JavaDoc is quite poor. There were dependency
problems there as well so we went with a release from 2020 to avoid
depending on a very recent Jenkins core.

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/EiffelBroadcasterConfig.java
@@ -25,6 +25,7 @@
 package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster;
 
 import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EventValidator;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.PossibleAuthenticationFailureException;
@@ -38,6 +39,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
@@ -88,6 +90,8 @@ public final class EiffelBroadcasterConfig extends Plugin implements Describable
     private String appId;
     /* A list of strings representing categories to include in the ActTs. */
     private final List<String> activityCategories = new ArrayList<>();
+
+    private final EventValidator eventValidator = new EventValidator();
 
     /**
      * Creates an instance with specified parameters.
@@ -338,6 +342,11 @@ public final class EiffelBroadcasterConfig extends Plugin implements Describable
     public void setActivityCategories(String activityCategories) {
         this.activityCategories.clear();
         this.activityCategories.addAll(Util.getLinesInString(activityCategories));
+    }
+
+    @Nonnull
+    public EventValidator getEventValidator() {
+        return eventValidator;
     }
 
     /**

commit 3574b014184afd63fe293f8f452f7dfbac4a0db6

EiffelBroadasterConfig: Mark eventValidator as transient
Attempting to serialize EiffelBroadcasterConfig#eventValidator will blow
up since one of the Jackson classes isn't deemed safe for serialization,
and that's totally fine. The EventValidator instance wasn't meant to be
serialized anyway.

This has been broken since commit e8171f6 but wasn't discovered since no
tests attempt to save the configuration to disk (maybe they should?) and
no manual testing prompted a configuration save.

Perhaps the event validator should be its own singleton instead of
piggy-packing on the EiffelBroadcasterConfig singleton. Let's think
about that when we revamp EiffelBroadcasterConfig to not extend the
deprecated Plugin class.

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/EiffelBroadcasterConfig.java
@@ -91,7 +91,7 @@ public final class EiffelBroadcasterConfig extends Plugin implements Describable
     /* A list of strings representing categories to include in the ActTs. */
     private final List<String> activityCategories = new ArrayList<>();
 
-    private final EventValidator eventValidator = new EventValidator();
+    private transient final EventValidator eventValidator = new EventValidator();
 
     /**
      * Creates an instance with specified parameters.