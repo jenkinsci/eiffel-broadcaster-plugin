jenkinsci/mq-notifier-plugin

Added toxiproxy to test bad connections

src/test/java/com/sonymobile/jenkins/plugins/mq/mqnotifier/ConnectionIntegrationTest.java
@@ -1,15 +1,25 @@
 package com.sonymobile.jenkins.plugins.mq.mqnotifier;
 
 import net.sf.json.JSONObject;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.ToxiproxyContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.testcontainers.containers.Network.newNetwork;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -21,29 +31,82 @@ import static org.junit.Assert.assertNotNull;
  */
 public class ConnectionIntegrationTest {
 
+    private static final String TOXIPROXY_NETWORK_ALIAS = "toxiproxy";
+
     RabbitMQContainer defaultMQContainer = TestUtil.getDefaultMQContainer();
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    @Rule
+    public Network network = newNetwork();
+
+    public ToxiproxyContainer toxiproxy = new ToxiproxyContainer()
+            .withNetwork(network)
+            .withNetworkAliases(TOXIPROXY_NETWORK_ALIAS);
 
     @Rule
     public TestRule chain = RuleChain
-            .outerRule(defaultMQContainer)
+            .outerRule(defaultMQContainer.withNetwork(network))
+            .around(toxiproxy)
             .around(new JenkinsRule());
+
+    /**
+     * Get the toxiproxy for the MQ container
+     */
+    private ToxiproxyContainer.ContainerProxy getProxy() {
+        return toxiproxy.getProxy(defaultMQContainer, TestUtil.PORT);
+    }
+
+    /**
+     * Creates a connection to rabbitMQ (through toxiproxy) and configure exchanges, passwords etc.
+     */
+    @Before
+    public void createProxyConnection() {
+        MQNotifierConfig config = MQNotifierConfig.getInstance();
+        assertNotNull("No config available: MQNotifierConfig", config);
+        TestUtil.setDefaultConfig(config);
+        config.setServerUri(formatProxyServerUri());
+
+        MQConnection conn = MQConnection.getInstance();
+        conn.initialize(config.getUserName(), config.getUserPassword(), config.getServerUri(), config.getVirtualHost());
+    }
+
+    /**
+     * Format an uri to the toxiproxy.
+     */
+    private String formatProxyServerUri() {
+        ToxiproxyContainer.ContainerProxy proxy = getProxy();
+        final String ipAddressViaToxiproxy = proxy.getContainerIpAddress();
+        final int portViaToxiproxy = proxy.getProxyPort();
+        return "amqp://" + ipAddressViaToxiproxy + ":" + portViaToxiproxy;
+    }
 
     /**
      * Test that the MQNotifier plugin sends messages correctly.
      */
     @Test
     public void testSentMessagesHasCorrectFormat() throws IOException, InterruptedException {
-        MQNotifierConfig config = MQNotifierConfig.getInstance();
-        assertNotNull("No config available: MQNotifierConfig", config);
-        TestUtil.setDefaultConfig(config);
-        config.setServerUri(defaultMQContainer.getAmqpUrl());
-
         MQConnection conn = MQConnection.getInstance();
-        conn.initialize(config.getUserName(), config.getUserPassword(), config.getServerUri(), config.getVirtualHost());
-
         ArrayList<JSONObject> expectedMessages = TestUtil.createMessages(10);
-        TestUtil.sendMessagesWithinTimeframe(expectedMessages, conn, 10);
+        expectedMessages.forEach(conn::publish);
         ArrayList<JSONObject> actualMessages = TestUtil.waitForMessages(conn, 10, 10, TestUtil.QUEUE_NAME);
+        assertEquals(expectedMessages, actualMessages);
+    }
+
+    /**
+     * Test that the MQNotifier won't lose messages when the connection is closed.
+     */
+    @Test
+    public void testSendMessagesHandlesClosedConnection() throws InterruptedException, IOException {
+        MQConnection conn = MQConnection.getInstance();
+        ArrayList<JSONObject> expectedMessages = TestUtil.createMessages(1000);
+
+        getProxy().setConnectionCut(true);
+        executor.submit(() -> {
+            expectedMessages.forEach(conn::publish);
+        });
+        Thread.sleep(1000);
+        getProxy().setConnectionCut(false);
+        ArrayList<JSONObject> actualMessages = TestUtil.waitForMessages(conn, 1000, 40, TestUtil.QUEUE_NAME);
         assertEquals(expectedMessages, actualMessages);
     }
 

jenkinsci/eiffel-broadcaster-plugin

src/test/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/ConnectionIntegrationTest.java
@@ -25,18 +25,24 @@
 package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster;
 
 import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
-import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EventValidationFailedException;
-import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.SchemaUnavailableException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.ToxiproxyContainer;
 
 import static org.junit.Assert.*;
+import static org.testcontainers.containers.Network.newNetwork;
 
 /**
  * Integration tests for the MQConnection
@@ -44,32 +50,104 @@ import static org.junit.Assert.*;
  * @author Hampus Johansson
  */
 public class ConnectionIntegrationTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionIntegrationTest.class);
+    private static final String TOXIPROXY_NETWORK_ALIAS = "toxiproxy";
     private static final int DEFAULT_MESSAGE_WAIT = 10;
 
     RabbitMQContainer defaultMQContainer = TestUtil.getDefaultMQContainer();
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    @Rule
+    public Network network = newNetwork();
+
+    private ToxiproxyContainer toxiproxy = new ToxiproxyContainer()
+            .withNetwork(network)
+            .withNetworkAliases(TOXIPROXY_NETWORK_ALIAS);
 
     @Rule
     public TestRule chain = RuleChain
-            .outerRule(defaultMQContainer)
+            .outerRule(defaultMQContainer.withNetwork(network))
+            .around(toxiproxy)
             .around(new JenkinsRule());
+
+    /**
+     * Get the toxiproxy for the MQ container
+     */
+    private ToxiproxyContainer.ContainerProxy getProxy() {
+        return toxiproxy.getProxy(defaultMQContainer, TestUtil.PORT);
+    }
+
+    /**
+     * Creates a connection to RabbitMQ through toxiproxy and configures exchanges, passwords etc.
+     */
+    @Before
+    public void createProxyConnection() {
+        EiffelBroadcasterConfig config = EiffelBroadcasterConfig.getInstance();
+        assertNotNull("No config available: EiffelBroadcasterConfig", config);
+        TestUtil.setDefaultConfig(config);
+        config.setServerUri(formatProxyServerUri());
+
+        MQConnection conn = MQConnection.getInstance();
+        conn.initialize(config.getUserName(), config.getUserPassword(), config.getServerUri(), config.getVirtualHost());
+    }
+
+    /**
+     * Format a URI to toxiproxy, which will be forwarded to RabbitMQ.
+     */
+    private String formatProxyServerUri() {
+        ToxiproxyContainer.ContainerProxy proxy = getProxy();
+        final String ipAddressViaToxiproxy = proxy.getContainerIpAddress();
+        final int portViaToxiproxy = proxy.getProxyPort();
+        return "amqp://" + ipAddressViaToxiproxy + ":" + portViaToxiproxy;
+    }
+
+    /**
+     * Publishes a single event and silently ignores any errors. Use this advisedly
+     * and only when a later stage of the test will reveal that the sending failed.
+     *
+     * @param event the event to publish
+     */
+    private void publishSilently(final EiffelEvent event) {
+        try {
+            Util.mustPublishEvent(event);
+        } catch (Exception e) {
+            LOGGER.error("Unexpected exception", e);
+        }
+    }
 
     /**
      * Test that the publisher sends messages correctly.
      */
     @Test
-    public void testSentMessagesHaveCorrectFormat()
-            throws EventValidationFailedException, IOException, InterruptedException, SchemaUnavailableException {
-        EiffelBroadcasterConfig config = EiffelBroadcasterConfig.getInstance();
-        assertNotNull("No config available: EiffelBroadcasterConfig", config);
-        TestUtil.setDefaultConfig(config);
-        config.setServerUri(defaultMQContainer.getAmqpUrl());
-
+    public void testSentMessagesHaveCorrectFormat() throws InterruptedException, IOException {
         MQConnection conn = MQConnection.getInstance();
-        conn.initialize(config.getUserName(), config.getUserPassword(), config.getServerUri(), config.getVirtualHost());
-
         int messageCount = 10;
         ArrayList<EiffelEvent> expectedMessages = TestUtil.createEvents(messageCount);
-        TestUtil.sendEventsWithinTimeframe(expectedMessages, DEFAULT_MESSAGE_WAIT);
+        expectedMessages.forEach(this::publishSilently);
+        ArrayList<EiffelEvent> actualMessages = TestUtil.waitForMessages(
+                conn,
+                messageCount,
+                DEFAULT_MESSAGE_WAIT,
+                TestUtil.QUEUE_NAME
+        );
+        assertEquals(expectedMessages, actualMessages);
+    }
+
+    /**
+     * Test that the publisher won't lose messages when the connection is closed.
+     */
+    @Test
+    public void testSendMessagesHandlesClosedConnection() throws InterruptedException, IOException {
+        MQConnection conn = MQConnection.getInstance();
+        int messageCount = 1000;
+        ArrayList<EiffelEvent> expectedMessages = TestUtil.createEvents(messageCount);
+
+        getProxy().setConnectionCut(true);
+        executor.submit(() -> {
+            expectedMessages.forEach(this::publishSilently);
+        });
+        Thread.sleep(1000);
+        getProxy().setConnectionCut(false);
         ArrayList<EiffelEvent> actualMessages = TestUtil.waitForMessages(
                 conn,
                 messageCount,