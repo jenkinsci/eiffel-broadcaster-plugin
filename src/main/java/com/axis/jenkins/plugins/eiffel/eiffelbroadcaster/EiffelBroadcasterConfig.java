jenkinsci/eiffel-broadcaster-plugin

commit bcc78f268fcebdafbbd00866df7e2ac94c246d9d

Introduce pluggable routing keys and Sepia conformance
Having all events sent with the same fixed routing key can force
consumers to consume more than they want to. Since a few years,
Sepia contains a recommended routing key format that we should be
able to conform to.

To ensure a smooth migration path for everyone, we replace
the EiffelBroadcasterConfig#routingKey string field with a
new #routingKeyProvider attribute that contains a subclass of
RoutingKeyProvider. We ship the plugin with two such implementations;
FixedRoutingKeyProvider that's equivalent to the legacy behavior,
and SepiaRoutingKeyProvider that follows Sepia. Old Jenkins instances
will get migrated to FixedRoutingKeyProvider while new ones get
SepiaRoutingKeyProvider by default. Other plugins may supply
their own implementations.

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/EiffelBroadcasterConfig.java
@@ -26,11 +26,15 @@ package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster;
 
 import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
 import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EventValidator;
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys.FixedRoutingKeyProvider;
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys.RoutingKeyProvider;
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys.SepiaRoutingKeyProvider;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.PossibleAuthenticationFailureException;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
+import hudson.ExtensionList;
 import hudson.XmlFile;
 import hudson.model.Descriptor;
 import hudson.util.FormValidation;
@@ -86,10 +90,18 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
     /* The virtual host which the connection intends to operate within. */
     private String virtualHost;
 
-    /* Messages will be sent with a routing key which allows messages to be delivered to queues that are bound with a
-     * matching binding key. The routing key must be a list of words, delimited by dots.
+    /**
+     * Legacy attribute that used to hold the AMQP routing key for outbound messages. The routing key is
+     * now determined by {@link #routingKeyProvider} and this attribute is kept only to please XStream.
      */
-    private String routingKey;
+    private transient String routingKey;
+
+    /**
+     * A reference to a {@link RoutingKeyProvider} that can figure out what routing key
+     * should be used for an outbound event.
+     */
+    private RoutingKeyProvider routingKeyProvider = new SepiaRoutingKeyProvider();
+
     /* Messages delivered to durable queues will be logged to disk if persistent delivery is set. */
     private boolean persistentDelivery = true;
     /* Application id that can be read by the consumer (optional). */
@@ -112,6 +124,17 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
         save();
         MQConnection.getInstance().initialize(userName, userPassword, serverUri, virtualHost);
         return true;
+    }
+
+    /** Migrates legacy model fields to the current model. Called by the XStream unmarshaler. */
+    protected Object readResolve() {
+        // If the legacy routingKey is set to something, migrate its contents
+        // to a FixedRoutingKeyProvider for functional equivalence.
+        if (StringUtils.isNotBlank(getRoutingKey())) {
+            setRoutingKeyProvider(new FixedRoutingKeyProvider(getRoutingKey()));
+            setRoutingKey(null);
+        }
+        return this;
     }
 
     /**
@@ -255,13 +278,32 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
     }
 
     /**
-     * Sets the routing key.
+     * Sets the routing key. This method exists for backwards compatibility reasons.
+     * Any non-null value will be passed to {@see #setRoutingKeyProvider(RoutingKeyProvider)}
+     * and this setter's underlying attribute will be set to null.
      *
      * @param routingKey the routing key.
      */
     @DataBoundSetter
     public void setRoutingKey(String routingKey) {
-        this.routingKey = routingKey;
+        if (routingKey != null) {
+            setRoutingKeyProvider(new FixedRoutingKeyProvider(routingKey));
+        }
+        this.routingKey = null;
+    }
+
+    /** Returns the currently configured {@link RoutingKeyProvider} implementation. */
+    @NonNull
+    public RoutingKeyProvider getRoutingKeyProvider() {
+        return this.routingKeyProvider;
+    }
+
+    /**
+     * Sets which {@link RoutingKeyProvider} implementation to use to generate routing keys for the events.
+     * */
+    @DataBoundSetter
+    public void setRoutingKeyProvider(@NonNull final RoutingKeyProvider routingKeyProvider) {
+        this.routingKeyProvider = routingKeyProvider;
     }
 
     /**
@@ -336,6 +378,10 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
         return eventValidator;
     }
 
+    public ExtensionList<RoutingKeyProvider.RoutingKeyProviderDescriptor> getRoutingKeyProviderDescriptors() {
+        return RoutingKeyProvider.RoutingKeyProviderDescriptor.all();
+    }
+
     @Override
     public String getDisplayName() {
         return "EiffelBroadcaster";
@@ -389,5 +435,14 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
             result = FormValidation.error("Invalid Uri");
         }
         return result;
+    }
+
+    /** Returns a custom help file location for fields where the Stapler doesn't locate it automatically. */
+    @Override
+    public String getHelpFile(final String fieldName) {
+        if ("routingKeyProvider".equals(fieldName)) {
+            return "/plugin/eiffel-broadcaster/help-routing-key-provider.html";
+        }
+        return super.getHelpFile(fieldName);
     }
 }

pom.xml
@@ -100,7 +100,7 @@
         <dependency>
             <groupId>io.jenkins</groupId>
             <artifactId>configuration-as-code</artifactId>
-            <scope>test</scope>
+            <optional>true</optional>
         </dependency>
         <dependency>
             <groupId>io.jenkins.configuration-as-code</groupId>

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/Util.java
@@ -113,34 +113,6 @@ public final class Util {
     }
 
     /**
-     * Publishes an event that has been transformed into a {@link JsonNode} and raises an exception if
-     * an error occurs.
-     *
-     * @return the published event or null if event publishing is disabled
-     * @throws EventValidationFailedException if the validation of the event against the JSON schema fails
-     * @throws JsonProcessingException if there's an error during JSON serialization
-     * @throws SchemaUnavailableException if there's no schema available for the supplied event
-     */
-    @CheckForNull
-    public static JsonNode mustPublishEvent(String eventName, String eventVersion, @NonNull final JsonNode eventJson)
-            throws EventValidationFailedException, JsonProcessingException, SchemaUnavailableException {
-        EiffelBroadcasterConfig config = EiffelBroadcasterConfig.getInstance();
-        if (config == null || !config.getEnableBroadcaster()) {
-            return null;
-        }
-        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
-                .appId(config.getAppId())
-                .deliveryMode(config.getPersistentDelivery() ? PERSISTENT_DELIVERY : NON_PERSISTENT_DELIVERY)
-                .contentType("application/json")
-                .timestamp(Calendar.getInstance().getTime())
-                .build();
-        config.getEventValidator().validate(eventName, eventVersion, eventJson);
-        MQConnection.getInstance().addMessageToQueue(config.getExchangeName(), config.getRoutingKey(),
-                props, new ObjectMapper().writeValueAsBytes(eventJson));
-        return eventJson;
-    }
-
-    /**
      * Publishes an {@link EiffelEvent} and raises an exception if an error occurs.
      *
      * @return the published event or null if event publishing is disabled
@@ -156,7 +128,16 @@ public final class Util {
             return null;
         }
         JsonNode eventJson = new ObjectMapper().valueToTree(event);
-        mustPublishEvent(event.getMeta().getType(), event.getMeta().getVersion(), eventJson);
+        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
+                .appId(config.getAppId())
+                .deliveryMode(config.getPersistentDelivery() ? PERSISTENT_DELIVERY : NON_PERSISTENT_DELIVERY)
+                .contentType("application/json")
+                .timestamp(Calendar.getInstance().getTime())
+                .build();
+        config.getEventValidator().validate(event.getMeta().getType(), event.getMeta().getVersion(), eventJson);
+        MQConnection.getInstance().addMessageToQueue(config.getExchangeName(),
+                config.getRoutingKeyProvider().getRoutingKey(event),
+                props, new ObjectMapper().writeValueAsBytes(eventJson));
         return eventJson;
     }
 

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/routingkeys/FixedRoutingKeyProvider.java
@@ -1,0 +1,88 @@
+/**
+ The MIT License
+
+ Copyright 2023 Axis Communications AB.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys;
+
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
+/**
+ * A {@link RoutingKeyProvider} implementation that always produces
+ * the same configurable fixed string regardless of the event input.
+ */
+public class FixedRoutingKeyProvider extends RoutingKeyProvider {
+    @NonNull
+    private String fixedRoutingKey;
+
+    @DataBoundConstructor
+    public FixedRoutingKeyProvider(@NonNull String fixedRoutingKey) {
+        this.fixedRoutingKey = fixedRoutingKey;
+    }
+
+    /** {@inheritDoc} */
+    @NonNull
+    @Override
+    public String getRoutingKey(final EiffelEvent event) {
+        return getFixedRoutingKey();
+    }
+
+    @NonNull
+    public String getFixedRoutingKey() {
+        return fixedRoutingKey;
+    }
+
+    @DataBoundSetter
+    public void setFixedRoutingKey(@NonNull String fixedRoutingKey) {
+        this.fixedRoutingKey = fixedRoutingKey;
+    }
+
+    @Override
+    public Descriptor<RoutingKeyProvider> getDescriptor() {
+        return Jenkins.get().getDescriptorByType(FixedRoutingKeyProvider.FixedRoutingKeyProviderDescriptor.class);
+    }
+
+    /** Descriptor for {@link FixedRoutingKeyProvider}. */
+    @Extension
+    public static class FixedRoutingKeyProviderDescriptor extends RoutingKeyProviderDescriptor {
+        public FormValidation doCheckFixedRoutingKey(@QueryParameter String value) {
+            if (StringUtils.isBlank(value)) {
+                return FormValidation.error("The routing key must be a non-empty string.");
+            }
+            return FormValidation.ok();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Fixed string";
+        }
+    }
+}

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/routingkeys/RoutingKeyProvider.java
@@ -1,0 +1,56 @@
+/**
+ The MIT License
+
+ Copyright 2023 Axis Communications AB.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys;
+
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
+import hudson.ExtensionList;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import java.io.Serializable;
+import jenkins.model.Jenkins;
+
+/**
+ * A routing key provider computes the AMQP routing key that should be used when publishing
+ * a given {@link EiffelEvent}.
+ */
+public abstract class RoutingKeyProvider implements Describable<RoutingKeyProvider>, Serializable {
+    /**
+     * Computes the routing key for an event.
+     *
+     * @param event the {@link EiffelEvent} whose routing key is wanted
+     * @return a string consisting of one or more dot-separated tokens that's usable as a routing key
+     */
+    public abstract String getRoutingKey(final EiffelEvent event);
+
+    /**
+     * Descriptor for {@link RoutingKeyProvider}s.
+     */
+    public abstract static class RoutingKeyProviderDescriptor extends Descriptor<RoutingKeyProvider> {
+        /** Obtain all registered {@link RoutingKeyProviderDescriptor}s. */
+        public static ExtensionList<RoutingKeyProviderDescriptor> all() {
+            return Jenkins.get().getExtensionList(RoutingKeyProviderDescriptor.class);
+        }
+    }
+}

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/routingkeys/SepiaRoutingKeyProvider.java
@@ -1,0 +1,93 @@
+/**
+ The MIT License
+
+ Copyright 2023 Axis Communications AB.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys;
+
+import com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.eiffel.EiffelEvent;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
+/**
+ * A routing key provider that produces AMQP routing keys that adhere to
+ * the <a href="https://eiffel-community.github.io/eiffel-sepia/rabbitmq-message-broker.html">message
+ * broker requirements in Sepia</a> (Sepia Eiffel Protocol Implementation Architecture).
+ */
+public class SepiaRoutingKeyProvider extends RoutingKeyProvider {
+    private String tag;
+
+    @DataBoundConstructor
+    public SepiaRoutingKeyProvider() { }
+
+    /** {@inheritDoc} */
+    @NonNull
+    @Override
+    public String getRoutingKey(final EiffelEvent event) {
+        return String.format("eiffel._.%s.%s.%s",
+                event.getMeta().getType(),
+                getRoutingKeyToken(getTag()),
+                getRoutingKeyToken(event.getMeta().getSource().getDomainId()));
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    @DataBoundSetter
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public Descriptor<RoutingKeyProvider> getDescriptor() {
+        return Jenkins.get().getDescriptorByType(SepiaRoutingKeyProviderDescriptor.class);
+    }
+
+    private String getRoutingKeyToken(String value) {
+        return StringUtils.isBlank(value) ? "_" : value;
+    }
+
+    /** Descriptor for {@link SepiaRoutingKeyProvider}. */
+    @Extension
+    public static class SepiaRoutingKeyProviderDescriptor extends RoutingKeyProviderDescriptor {
+        public FormValidation doCheckTag(@QueryParameter String value) {
+            if (value.contains(".")) {
+                return FormValidation.error("The tag must not contain any dots since that would "
+                        + "make the resulting routing key ambiguous.");
+            }
+            return FormValidation.ok();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Sepia";
+        }
+    }
+}

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/routingkeys/SepiaRoutingKeyProviderConfigurator.java
@@ -1,0 +1,90 @@
+/**
+ The MIT License
+
+ Copyright 2023 Axis Communications AB.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+package com.axis.jenkins.plugins.eiffel.eiffelbroadcaster.routingkeys;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import io.jenkins.plugins.casc.BaseConfigurator;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.Configurator;
+import io.jenkins.plugins.casc.ConfiguratorException;
+import io.jenkins.plugins.casc.model.CNode;
+import io.jenkins.plugins.casc.model.Mapping;
+import java.util.Collections;
+import java.util.List;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * A {@link Configurator} implementation that takes care of CasC serialization/deserialization
+ * of {@link SepiaRoutingKeyProvider} objects. CasC tries very hard to not serialize any default
+ * values or empty objects, so unless you've set the optional Tag field to a non-empty value it
+ * won't serialize the SepiaRoutingKeyProvider object at all, causing us to lose information about
+ * which class was chosen as the routing key provider. This configurator works around this by
+ * always adding a dummy key/value pair during serialization.
+ */
+@Extension(optional = true)
+public class SepiaRoutingKeyProviderConfigurator extends BaseConfigurator<SepiaRoutingKeyProvider> {
+    /** The name of the configuration key used for {@link SepiaRoutingKeyProvider#getTag()}. */
+    private static final String KEY_TAG = "tag";
+
+    /** The name of the configuration key used for the dummy value used to avoid optimization. */
+    private static final String KEY_DUMMY = "dummy";
+
+    @Override
+    protected SepiaRoutingKeyProvider instance(Mapping mapping, ConfigurationContext context) throws ConfiguratorException {
+        SepiaRoutingKeyProvider provider = new SepiaRoutingKeyProvider();
+        if (mapping == null) {
+            return provider;
+        }
+        provider.setTag(mapping.get(KEY_TAG) != null ? mapping.getScalarValue(KEY_TAG) : "");
+        return provider;
+    }
+
+    @Override
+    public boolean canConfigure(Class clazz) {
+        return clazz == SepiaRoutingKeyProvider.class;
+    }
+
+    @CheckForNull
+    @Override
+    public CNode describe(SepiaRoutingKeyProvider instance, ConfigurationContext context) throws Exception {
+        Mapping mapping = new Mapping();
+        mapping.put(KEY_DUMMY, "dummy value to make JCasC behave the way we want");
+        mapping.put(KEY_TAG, StringUtils.defaultIfBlank(instance.getTag(), ""));
+        return mapping;
+    }
+
+    @Override
+    public Class<SepiaRoutingKeyProvider> getTarget() {
+        return SepiaRoutingKeyProvider.class;
+    }
+
+    @NonNull
+    @Override
+    public List<Configurator<SepiaRoutingKeyProvider>> getConfigurators(ConfigurationContext context) {
+        return Collections.singletonList(this);
+    }
+}

src/main/resources/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/EiffelBroadcasterConfig/config.groovy
@@ -50,10 +50,9 @@ f.section(title: "Eiffel Broadcaster Plugin") {
     f.entry(title: "Virtual host", field: "virtualHost", help: l+"help-virtual-host.html") {
         f.textbox("value":instance.virtualHost)
     }
-    f.entry(title: "Routing Key", field: "routingKey", help: l+"help-routing-key.html") {
-        f.textbox("value":instance.routingKey)
-    }
     f.advanced() {
+        f.dropdownDescriptorSelector(title: "Routing key algorithm",
+                field: "routingKeyProvider", descriptors: descriptor.routingKeyProviderDescriptors)
         f.entry(title: "Application Id", field: "appId", help: l+"help-application-id.html") {
             f.textbox("value":instance.appId)
         }

commit a6bb71837791526dc49cb7fe67db0b8ac954def2

Correct bad Javadoc syntax
Commit bcc78f2 mistakenly introduced a Javadoc markup bug (@see was
used instead of @link) which wasn't discovered until Javadoc was run
during the release process for 2.7.0.

We should run "mvn javadoc:javadoc" during the pre-merge CI execution
but we're inheriting its configuration so it's not immediately clear
how to accomplish this.

src/main/java/com/axis/jenkins/plugins/eiffel/eiffelbroadcaster/EiffelBroadcasterConfig.java
@@ -279,7 +279,7 @@ public final class EiffelBroadcasterConfig extends GlobalConfiguration {
 
     /**
      * Sets the routing key. This method exists for backwards compatibility reasons.
-     * Any non-null value will be passed to {@see #setRoutingKeyProvider(RoutingKeyProvider)}
+     * Any non-null value will be passed to {@link #setRoutingKeyProvider(RoutingKeyProvider)}
      * and this setter's underlying attribute will be set to null.
      *
      * @param routingKey the routing key.